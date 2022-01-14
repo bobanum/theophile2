52145e63c8238bbf5177fdb68100eac1df1bd474
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2022-01-14 18:41:37 -0500

    Able to navigate from one slide to another (with keyboard and mouse)

--- src/js/Slide.js
+++ src/js/Slide.js
@@ -44,11 +44,12 @@ export default class Slide extends Plugin {
      * @memberof Slide
      */
     get first() {
-        var ptr = this;
-        while (ptr.previous) {
-            ptr = ptr.previous;
-        }
-        return ptr;
+        if (!this.previous) return this;
+        return this.previous.first;
+    }
+    get last() {
+        if (!this.next) return this;
+        return this.next.last;
     }
     get html() {
         if (!this._html) {
@@ -62,33 +63,139 @@ export default class Slide extends Plugin {
         result.classList.add("th-slide");
         result.appendChild(this.html_header());
         result.appendChild(this.html_footer());
+        result.appendChild(this.html_body());
+        return result;
+    }
+    html_body() {
+        const result = document.createElement("div");
+        result.classList.add("th-slide-body");
         this.contents.forEach(content => {
             result.appendChild(content.cloneNode(true));
         });
         return result;
     }
     static html_backdrop() {
-        const result = document.createElement("div");
-        result.classList.add("th-slide-backdrop");
-        const navigation = result.appendChild(document.createElement("div"));
+        const backdrop = document.createElement("div");
+        backdrop.classList.add("th-slide-backdrop");
+        const navigation = backdrop.appendChild(document.createElement("div"));
         navigation.classList.add("th-slide-navigation");
         const previous = navigation.appendChild(document.createElement("div"));
         previous.classList.add("th-slide-previous");
+        previous.addEventListener("click", e => {
+            const previous = backdrop.slide.previous;
+            if (previous) {
+                backdrop.appendChild(previous.html);
+                backdrop.slide.html.remove();
+                backdrop.slide = previous;
+            }
+        });
         const next = navigation.appendChild(document.createElement("div"));
         next.classList.add("th-slide-next");
+        next.addEventListener("click", e => this.showNext());
         const first = navigation.appendChild(document.createElement("div"));
         first.classList.add("th-slide-first");
+        first.addEventListener("click", e => {
+            const first = backdrop.slide.first;
+            if (first) {
+                backdrop.appendChild(first.html);
+                backdrop.slide.html.remove();
+                backdrop.slide = first;
+            }
+        });
         const last = navigation.appendChild(document.createElement("div"));
         last.classList.add("th-slide-last");
+        last.addEventListener("click", e => {
+            const last = backdrop.slide.last;
+            console.log(last);
+            if (last) {
+                backdrop.appendChild(last.html);
+                backdrop.slide.html.remove();
+                backdrop.slide = last;
+            }
+        });
         var options = navigation.appendChild(document.createElement("div"));
         options.classList.add("th-slide-options");
+        var option = options.appendChild(document.createElement("span"));
+        option.classList.add("th-option-menu");
         var option = options.appendChild(document.createElement("span"));
         option.classList.add("th-option-contactsheet");
         var option = options.appendChild(document.createElement("span"));
         option.classList.add("th-option-stopslideshow");
         var option = options.appendChild(document.createElement("span"));
         option.classList.add("th-option-print");
-        return result;
+        this.keydown(backdrop);
+        backdrop.Slide = this;
+        return backdrop;
+    }
+    static keydown(backdrop) {
+        backdrop.tabIndex = "0";
+        backdrop.addEventListener("keydown", e => {
+            if (e.key === "Control" || e.key === "Alt" || e.key === "Shift" || e.key === "Meta") {
+                return;
+            }
+            var prefix = "";
+            if (e.altKey) prefix += "Alt-";
+            if (e.ctrlKey || e.metaKey) prefix += "Ctrl-";
+            if (e.shiftKey) prefix += "Shift-";
+            var key = prefix + e.key;
+            // var code = prefix + e.code;
+            // console.log(key);
+            switch (key) {
+                case "ArrowRight":
+                case "ArrowDown":
+                case "PageDown":
+                case "+":
+                case "Enter":
+                    this.showNext();
+                    break;
+                case "ArrowLeftt":
+                case "ArrowUp":
+                case "PageUp":
+                case "-":
+                    this.showPrevious();
+                    break;
+                case "Home":
+                    this.showFirst();
+                    break;
+                case "End":
+                    this.showLast();
+                    break;
+                case " ":
+                case "Escape":
+                    this.startSlideshow(false);
+            }
+            e.stopPropagation();
+        });
+    }
+    static showSlide(slide) {
+        if (slide === this.backdrop.slide) return;
+        this.backdrop.appendChild(slide.html);
+        this.backdrop.slide.html.remove();
+        this.backdrop.slide = slide;
+    }
+    static showNext(n = 1) {
+        var slide = this.backdrop.slide;
+        while (n > 0 && slide.next) {
+            slide = slide.next;
+            n -= 1;
+        }
+        this.showSlide(slide);
+    }
+    static showPrevious(n = 1) {
+        var slide = this.backdrop.slide;
+        while (n > 0 && slide.previous) {
+            slide = slide.previous;
+            n -= 1;
+        }
+        this.showSlide(slide);
+    }
+    static showLast() {
+        var slide = this.backdrop.slide.last;
+        this.showSlide(slide);
+    }
+    static showFirst() {
+        var slide = this.backdrop.slide.first;
+        this.showSlide(slide);
     }
     html_header() {
         var ptr = this;
@@ -173,7 +280,7 @@ export default class Slide extends Plugin {
         var contactsheet = document.createElement("section");
         contactsheet.classList.add("th-contactsheet");
         var slide = this.first;
-        while(slide) {
+        while (slide) {
             contactsheet.appendChild(slide.html.cloneNode(true));
             slide = slide.next;
         }
@@ -201,29 +308,40 @@ export default class Slide extends Plugin {
         // }
         return this.slides;
     }
+    static get state() {
+        return document.body.classList.contains("th-slideshow");
+    }
+    static startSlideshow(state = true) {
+        if (state) {
+            document.body.classList.add("th-slideshow");
+            this.backdrop = document.body.appendChild(this.html_backdrop());
+            this.backdrop.slide = this.slides[0]
+            this.backdrop.appendChild(this.backdrop.slide.html);
+            localStorage.slideshow = "true";
+            setTimeout(() => {
+                this.backdrop.focus();
+            }, 100);
+        } else {
+            document.body.classList.remove("th-slideshow");
+            this.backdrop.remove();
+            delete this.backdrop;
+            localStorage.slideshow = "false";
+        }
+        return this;
+    }
     static async clean() {
         window.addEventListener("keydown", e => {
-            console.log(e);
             if (e.key === "Shift" || e.key === "Control" || e.key === "Alt" || e.key === "Meta") return;
             if (e.code === "Space") {
                 // var visible = this.getVisible();
-                var state = document.body.classList.contains("th-slideshow");
-                if (state) {
-                    document.body.classList.remove("th-slideshow");
-                    document.body.querySelector(".th-slide-backdrop").remove();
-                } else {
-                    document.body.classList.add("th-slideshow");
-                    var backdrop = document.body.appendChild(this.html_backdrop());
-                    console.log(this.slides);
-                    console.log(this.slides[0]);
-                    backdrop.appendChild(this.slides[0].html);
-                    // console.log(visible);
-                    // e.preventDefault();
-                }
+                this.startSlideshow();
                 e.stopPropagation();
                 return false;
             }
         });
+        if (localStorage.slideshow === "true") {
+            this.startSlideshow();
+        }
     }
     static init() {
         super.init();
