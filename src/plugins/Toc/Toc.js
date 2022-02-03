459a36ac36e95d2c2d00f24c4e96708e55fde5c2
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2022-02-03 12:47:03 -0500

    Toc : Bug when currentLevel === level fixed

--- src/plugins/Toc/Toc.js
+++ src/plugins/Toc/Toc.js
@@ -1,11 +1,12 @@
 import Plugin from "../Plugin.js";
 //TODO #23 When skipping headings (like h1+h3) collapsing doesn't work. Don't have the time to fix now
 //TODO #24 Make scrolling smouth. For now, il scroll-behavious is smouth, page scrolls from top after ending slideshow.
+//TODO #25 Toc: Make it possible to pin TOC on the page
 export default class Toc extends Plugin {
     static async process() {
         await super.process();
-        const headings = document.body.querySelectorAll("h1,h2,h3");
-        this.hierarchy = this.getHierarchy(headings);
+        this.headings = Array.from(document.body.querySelectorAll("h1,h2,h3"));
+        this.hierarchy = this.getHierarchy(this.headings);
     }
     static async afterMount() {
         await super.afterMount();
@@ -22,6 +23,8 @@ export default class Toc extends Plugin {
             const li = result.appendChild(document.createElement("li"));
             li.classList.add("th-toc-level-" + level);
             if (headingObject.heading) {
+                headingObject.heading.tocElement = li;
+                li.destination = headingObject.heading;
                 const div = li.appendChild(document.createElement("div"));
                 const a = div.appendChild(document.createElement("a"));
                 a.href = "#" + headingObject.heading.id;
@@ -36,33 +39,56 @@ export default class Toc extends Plugin {
     static getHierarchy(nodeList) {
         const result = [];
         var currentLevel = 0;
-        var currentGroup = result;
-        const path = [];
+        const path = [result];
         nodeList.forEach(heading => {
             let level = parseInt(heading.tagName[1]);
             const headingObject = { heading: heading, group: [] };
             while (level > currentLevel + 1) {
                 const empty = { heading: null, group: [] };
-                currentGroup.push(empty);
-                path[currentLevel] = currentGroup;
-                currentGroup = empty.group;
+                path[currentLevel].push(empty);
                 currentLevel += 1;
+                path[currentLevel] = empty.group;
             }
             if (level === currentLevel) {
-                currentGroup.push(headingObject);
+                path[currentLevel - 1].push(headingObject);
                 path[currentLevel] = headingObject.group;
             } else if (level < currentLevel) {
                 currentLevel = level;
-                currentGroup = path[currentLevel - 1];
-                currentGroup.push(headingObject);
+                path[currentLevel - 1].push(headingObject);
                 path[currentLevel] = headingObject.group;
             } else if (level === currentLevel + 1) {
-                currentGroup.push(headingObject);
-                path[currentLevel] = currentGroup;
-                currentGroup = headingObject.group;
+                path[currentLevel].push(headingObject);
                 currentLevel = level;
+                path[currentLevel] = headingObject.group;
             }
         });
         return result;
     }
+	static findVisibleHeading() {
+		var headings = this.headings.map(heading => {
+			return [heading, heading.getBoundingClientRect().y];
+		}).sort((a, b) => (a[1] < b[1] ? -1 : 1));
+		var last = headings.slice(-1)[0];
+		headings = headings.filter(heading => heading[1] >= 0);
+		return (headings[0] || last)[0];
+	}
+	static async clean() {
+		super.clean();
+        delete this.hierarchy;
+		window.addEventListener("scroll", e => {
+			const visible = this.findVisibleHeading();
+            if (visible.tocElement.classList.contains("th-toc-current")) {
+                return;
+            }
+            document.querySelectorAll(".th-toc-current, .th-toc-current-within").forEach(element => element.classList.remove("th-toc-current", "th-toc-current-within"));
+            visible.tocElement.classList.add("th-toc-current");
+            var ptr = visible.tocElement;
+            while (ptr) {
+                if (ptr.id === "th-toc") break;
+                ptr.classList.add("th-toc-current-within");
+                ptr = ptr.parentNode.closest("li");
+            }
+
+		});
+	}
 }
